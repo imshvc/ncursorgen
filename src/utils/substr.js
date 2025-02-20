fa5ed66a42817b9fd552420ddac183aed2e6adbf
Nurudin Imsirovic <realnurudinimsirovic@gmail.com>
2025-02-20 09:41:41 +0100

    strip substr unnecessarily complex logic

--- src/utils/substr.js
+++ src/utils/substr.js
@@ -7,29 +7,6 @@
  * @param {number} length Length of the returned string
  * @returns {string}
  */
-function substr(string, offset, length = -1) {
-  // handle case: length not specified
-  if (0 > length) {
-    length = string.length;
-  }
-
-  // handle case: out of bounds offset
-  if (offset > string.length) {
-    return '';
-  }
-
-  length += offset;
-
-  // mitigate against overflow
-  if (length > string.length) {
-    length = string.length;
-  }
-
-  let buffer = '';
-
-  for (let i = offset; i < length; i++) {
-    buffer += string[i];
-  }
-
-  return buffer;
+function substr(string, offset, length = 0) {
+  return string.substring(offset, offset + length);
 }
